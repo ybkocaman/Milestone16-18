550065d0ad457efc5764cd6214804fea90d07447
Yusuf <ybkocaman@gmail.com>
2024-01-16 09:56:05 +0000

    Initial Commit

---
+++ Milestone16-18/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milestone16-18
+//
+//  Created by Yusuf Burak on 16/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Milestone16-18/Milestone16_18App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone16_18App.swift
+//  Milestone16-18
+//
+//  Created by Yusuf Burak on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Milestone16_18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
